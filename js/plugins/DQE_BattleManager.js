7ce63680ecd35e69916820a41312c62dadbe7c60
NotADev9000 <notadev9000@gmail.com>
2020-10-16 21:56:56 +0100

    Battle: battle start messages

--- js/plugins/DQE_BattleManager.js
+++ js/plugins/DQE_BattleManager.js
@@ -28,6 +28,30 @@ DQEng.Battle_Manager = DQEng.Battle_Manager || {};
 // Battle_Manager
 //-----------------------------------------------------------------------------
 
+/**
+ * Displays emerge messages depending on enemy size & type
+ * 
+ * NOTE: If an 'appear halfway' enemy is first in the troop
+ * these messages may not display correctly so always add hidden
+ * enemies last
+ */
+BattleManager.displayStartMessages = function () {
+    var groups = $gameTroop.groups();
+    if (groups.length > 1) {
+        $gameMessage.add('A group of enemies appears!');
+    } else if (groups[0].enemies.length > 1) {
+        $gameMessage.add(`A group of ${groups[0].name}s appears!`);
+    } else {
+        $gameMessage.add(TextManager.emerge.format(groups[0].name));
+    }
+
+    if (this._preemptive) {
+        $gameMessage.add(TextManager.preemptive.format($gameParty.name()));
+    } else if (this._surprise) {
+        $gameMessage.add(TextManager.surprise.format($gameParty.name()));
+    }
+};
+
 BattleManager.refreshStatus = function () {
     this._statusWindow.forEach(statusWindow => {
         statusWindow.refresh();
