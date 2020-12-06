3ec19faebbe6486b41d9645971e5a568ab11e3bb
NotADev9000 <notadev9000@gmail.com>
2020-12-06 13:48:32 +0000

    TODO

--- TODO.js
+++ TODO.js
@@ -1,18 +1,17 @@
-/* TODO list in any order
+/* TODO list in any order (high priority 1 -> 4 low priority)
 *
-* . Equipment menu
-* . Status menu
-* . Save/Load menu
-* . Option menu
-* . Misc menu (heal all, line-up, settings (text speed), current objective, battle music...)
-* . Battle Misc (battle text speed...)
-* . Death doesn't cause gameover but leads to last church used and losing half your gold
-* . Actor stats
-* . Quest system
-* . Skill tree system
-* . Shop system
-* . Bank system
-* . States/Elements/Skills/Abilities
-* . Battle formulas (crit, evasion...)
-* . Battleback match terrain
+* . Status menu 1
+* . Save/Load menu 2
+* . Option menu 2
+* . Misc menu (heal all, line-up, settings (text speed), current objective, battle music...) 2
+* . Battle Misc (battle text speed...) 3
+* . Death doesn't cause gameover but leads to last church used and losing half your gold 2
+* . Actor stats 3
+* . Quest system 4
+* . Skill tree system 4
+* . Shop system 4
+* . Bank system 4
+* . States/Elements/Skills/Abilities 1
+* . Battle formulas (crit, evasion...) 1
+* . Battleback match terrain 3
 */
